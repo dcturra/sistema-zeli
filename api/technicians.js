a9ce3c27798806e80b57e8e0a8ca97ba4d6db2a5
Diogo da Costa Turra <dcturra@gmail.com>
2025-07-31 09:39:38 -0300

    fix: melhora headers CORS para resolver problema de autenticação

--- api/technicians.js
+++ api/technicians.js
@@ -4,7 +4,8 @@ const { kv } = require('@vercel/kv');
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Max-Age', '86400');
 
     if (req.method === 'OPTIONS') {
         res.status(200).end();
